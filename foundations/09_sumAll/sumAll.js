87a6c886f114d8b209105bb61f9fd740e53d6c2a
SilasNS <silasschlax@gmail.com>
2026-03-31 15:23:50 -0400

    Completed Foundations JS exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (
+    !Number.isInteger(num1) || 
+    num1 < 0 ||
+    !Number.isInteger(num2) ||
+    num2 < 0
+  ) return "ERROR"
 
+  let smaller, larger, sum = 0
+  if (num1 > num2) smaller = num2, larger = num1
+  else smaller = num1, larger = num2
+
+  for (let i = smaller; i <= larger; i++) {
+    sum += i
+  }
+  return sum
 };
 
 // Do not edit below this line
