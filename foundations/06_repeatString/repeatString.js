cd9992100a7c506f61055e7f05425d59cc4a7822
SilasNS <silasschlax@gmail.com>
2026-03-31 14:29:57 -0400

    Completed Foundations JS exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, ittr) {
+  if (ittr < 0) return "ERROR"
+  let newString = ""
+  for (ittr; ittr > 0; ittr--) {
+    newString = newString.concat(string)
+  }
+  return newString
 };
 
+repeatString("hey", 3)
 // Do not edit below this line
 module.exports = repeatString;
