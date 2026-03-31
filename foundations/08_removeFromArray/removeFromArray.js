9a2c40c0378df19a51f0926a047c38d26809fd45
SilasNS <silasschlax@gmail.com>
2026-03-31 15:14:24 -0400

    Completed Foundations JS exercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(ary) {
+  for (let i = arguments.length - 1; i > 0; i--) {
+    ary = removeArg(ary, arguments[i])
+  }
+  return ary
 };
+
+function removeArg(ary, removeArg) {
+  for (let i = 0; i < ary.length; i++) {
+    console.log(ary[i])
+    if (ary[i] === removeArg) {
+      ary.splice(i, 1)
+      i--
+    }
+  }
+  return ary
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
