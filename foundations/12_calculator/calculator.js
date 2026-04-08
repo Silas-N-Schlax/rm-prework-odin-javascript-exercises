cd5d1ac7be3365590886c8010551333b183cc525
SilasNS <silasschlax@gmail.com>
2026-04-08 10:15:19 -0400

    Completed Foundations JS exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(ary) {
+	return ary.reduce((total, num) => {
+    return total += num
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(ary) {
+  return ary.reduce((total, num) => {
+    console.log(`${total}, current: ${num}`)
+    return total = total * num
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1
+  for (let i = num; i > 0; i--) {
+    total = total * i
+  }
+  return total
 };
 
 // Do not edit below this line
