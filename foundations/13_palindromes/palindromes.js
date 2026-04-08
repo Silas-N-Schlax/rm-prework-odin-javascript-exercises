1d781d15b16b04aff8ec97e58897f79c0066d309
SilasNS <silasschlax@gmail.com>
2026-04-08 10:43:40 -0400

    Completed Foundations JS exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let sanitizedString = string.split("").filter(
+    char => 
+      char != ' ' &&
+      char != ',' &&
+      char != '.' &&
+      char != '!'
+  ).join("").toLowerCase()
+  let reversedString = sanitizedString.split("").reverse().join("")
+  
+  return reversedString === sanitizedString ? true : false
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
