9f11c80cc9b7604a9d7c033154d8bdc20e006f88
SilasNS <silasschlax@gmail.com>
2026-04-08 11:15:30 -0400

    Completed Foundations JS exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+  let sortedPeople = obj.sort((first, second) => {
+    firstAge = calculateAge(first.yearOfBirth, first.yearOfDeath)
+    secondAge = calculateAge(second.yearOfBirth, second.yearOfDeath)
+    return secondAge - firstAge
+  })
+  return sortedPeople[0]
 };
+
+function calculateAge(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear()
+  }
+  return death - birth
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
