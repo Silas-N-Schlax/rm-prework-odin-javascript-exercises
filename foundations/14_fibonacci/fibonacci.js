ebd420ef488c33c8712862850461cbe5ad7178fa
SilasNS <silasschlax@gmail.com>
2026-04-08 10:54:35 -0400

    Completed Foundations JS exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+  index = Number(index)
+  if (index < 0) return "OOPS";
+  if (index === 0) return 0;
+  const sqe = [1, 1] // sequence
+  for (let i = 2; i < index; i++) {
+    sqe.push(sqe[i-2] + sqe[i-1])
+  }
+  return sqe[index - 1]
 };
 
 // Do not edit below this line
